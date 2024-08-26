a223ef6632c051af5fab932eeeab08ed235445ab
Ryan Petersen <rp26898@gmail.com>
2024-08-26 09:10:22 -0700

    updated my resume.jsx page

---
+++ src/pages/Resume.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "../styles/style.css";
+
+function Resume() {
+  return (
+    <div className="resume-container">
+      <h1>Resume</h1>
+      <div className="coming-soon">
+        <img
+          src="\imgs\Coming soon.png"
+          alt="Coming Soon"
+          className="coming-soon-image"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Resume;
